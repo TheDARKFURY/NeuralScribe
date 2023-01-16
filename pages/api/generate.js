02452e1481e0e75672b5bc990ff483eaf887bbac
Soham Shah <sohamshah419@gmail.com>
2023-01-16 14:33:00 +0530

    prompt-chaining added

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,23 +6,74 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = `Write me a interesting story in the style of Wednesday Addams with the title below. Please make sure the blog post goes in-depth on the topic and shows that the writer did their research.
+// const basePromptPrefix = `Write me a interesting story in the style of Sheldon Cooper with the title below. Please make sure the blog post goes in-depth on the topic and shows that the writer did their research.
 
-Title:`;
+// Title:`;
+// const generateAction = async (req, res) => {
+//   // Run first prompt
+//   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+//     temperature: 0.7,
+//     max_tokens: 2500,
+//   });
+  
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+
+//   res.status(200).json({ output: basePromptOutput });
+// };
+
+// export default generateAction;
+
+
+const basePromptPrefix =
+`
+Write me a detailed table of contents for a blog post with the title below.
+
+Title:
+`
+
 const generateAction = async (req, res) => {
-  // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.7,
-    max_tokens: 2500,
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 250,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the table of contents and title of the blog post below and generate a blog post written in the style of Sheldon Cooper. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
+
+  Title: ${req.body.userInput}
+
+  Table of Contents: ${basePromptOutput.text}
+
+  Blog Post:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
 };
 
 export default generateAction;
